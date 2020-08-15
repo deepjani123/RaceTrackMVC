18840a44cc4d24bb2c9f329c573f84344f4c8d25
Deep Jani <deepjani@hotmail.com>
2020-08-15 11:38:02 -0400

    first commit

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RaceTrackMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
