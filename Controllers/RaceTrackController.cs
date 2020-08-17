eb43bbb678ac160846b37fa7a859a38f83c7c83c
Deep Jani <deepjani@hotmail.com>
2020-08-17 11:40:55 -0400

    display vehicle on track

--- Controllers/RaceTrackController.cs
+++ Controllers/RaceTrackController.cs
@@ -14,6 +14,8 @@ namespace RaceTrackMVC.Controllers
         
         public Vehicle Vehicle { get; set; }
 
+        public VehicleTrack VehicleTrack { get; set; }
+
         public RaceTrackController(TrackDBContext trackDBContext)
         {
             _trackDBContext = trackDBContext;
@@ -51,6 +53,79 @@ namespace RaceTrackMVC.Controllers
             //to clear previous form data
             ModelState.Clear();
             return View("Index");
+        }
+
+
+        [HttpPost]
+        public ActionResult AddToTrack(int? VehicleId)
+        {
+            VehicleTrack = new VehicleTrack();
+
+            int total = _trackDBContext.VehicleTracks.Count();
+
+            if(total>=5)
+            {
+                return Json(new { success = false, message = "race track is full!" });
+            }
+
+            var vehicle = _trackDBContext.VehicleTracks.FirstOrDefault(u => u.VehicleId == VehicleId);
+
+            if(vehicle!=null)
+            {
+                return Json(new { success = false, message = "this vehicle is already on track!" });
+            }
+            else
+            {
+                VehicleTrack.VehicleId = (int)VehicleId;
+                VehicleTrack.TrackId = 1;
+                _trackDBContext.VehicleTracks.Add(VehicleTrack);
+                _trackDBContext.SaveChanges();
+            }
+
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ActionResult GetVehicleOnTrack()
+        {
+
+            string trackName = (from t in _trackDBContext.Tracks where t.Id == 1 select t.Name).FirstOrDefault();
+            var query = from vt in _trackDBContext.VehicleTracks
+                        from t in _trackDBContext.Tracks
+                        select new
+                        {
+                            vehicleId = vt.VehicleId,
+                            vehicleName = vt.Vehicle.Name,
+                            trackName = trackName
+                        };
+
+            
+
+            return Json(new { data = query.ToList() }, JsonRequestBehavior.AllowGet);
+
+
+        }
+
+
+        [HttpPost]
+        public ActionResult RemoveFromTrack(int? VehicleId)
+        {
+            
+
+            VehicleTrack = _trackDBContext.VehicleTracks.FirstOrDefault(u => u.VehicleId == VehicleId);
+
+            if (VehicleTrack == null)
+            {
+                return Json(new { success = false, message = "vehicle not found!" });
+            }
+            else
+            {
+                _trackDBContext.VehicleTracks.Remove(VehicleTrack);
+                _trackDBContext.SaveChanges();
+            }
+
+            return Json(new { success = false, message = "Removed from track!" });
+
         }
     }
 }
